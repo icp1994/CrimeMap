43160199d3f704d4aed742cc42f2b690c03732c3
icp1994 <roy.arnab387@gmail.com>
2016-04-21 18:31:03 +0530

    databse setup

---
+++ db_setup.py
@@ -0,0 +1,32 @@
+# -*- file-encoding: utf-8 -*-
+"""
+Created on April 21, 2016 at 17:53
+
+@author: carl
+"""
+
+import pymysql
+import dbconfig
+
+connection = pymysql.connect(host='localhost',
+                             user=dbconfig.db_user,
+                             passwd=dbconfig.db_password)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+        cursor.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+        id int NOT NULL AUTO_INCREMENT,
+        latitude FLOAT(10, 6),
+        longitude FLOAT(10, 6),
+        date DATETIME,
+        category VARCHAR(50),
+        description VARCHAR(1000),
+        update_at TIMESTAMP,
+        PRIMARY KEY (id)
+        )"""
+        cursor.execute(sql)
+    connection.commit()
+finally:
+    connection.close()
